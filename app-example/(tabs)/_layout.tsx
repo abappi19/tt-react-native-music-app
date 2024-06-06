89bb3fcd943844386f412aefe0f3a5d1cd3762cc
Bappi <abappi@tulip-tech.com>
2024-06-06 20:05:55 +0600

    project reset

---
+++ app-example/(tabs)/_layout.tsx
@@ -0,0 +1,23 @@
+import { Tabs } from 'expo-router';
+import React from 'react';
+
+import { TabBarIcon } from '@/components/navigation/TabBarIcon';
+import { Colors } from '@/constants/Colors';
+import { useColorScheme } from '@/hooks/useColorScheme';
+
+export default function TabLayout() {
+  const colorScheme = useColorScheme();
+
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+        headerShown: false,
+      }}>
+      <Tabs.Screen name="favorite" options={{ headerShown: false }} />
+      <Tabs.Screen name="playlist" options={{ headerShown: false }} />
+      <Tabs.Screen name="explore" options={{ headerShown: false }} />
+      <Tabs.Screen name="artists" options={{ headerShown: false }} />
+    </Tabs>
+  );
+}
