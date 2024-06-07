812620549c0ce9b0c9028202a5a68e3f8aa49d2c
Bappi <abappi@tulip-tech.com>
2024-06-07 22:04:51 +0600

    animated playback effect added using react-native-loader-kit

--- src/components/track-list-item.tsx
+++ src/components/track-list-item.tsx
@@ -2,10 +2,11 @@ import { unknownTrackImageUri } from "@/constants/images";
 import { colors, fontSize } from "@/constants/tokens";
 import { MaterialIcons } from "@expo/vector-icons";
 import { Image } from "expo-image";
-import { Text, TouchableHighlight, TouchableOpacity, View } from "react-native";
-import { Track, useActiveTrack } from "react-native-track-player";
-import { StopPropagation } from "./utils/stop-propagation";
+import { Text, TouchableHighlight, View } from "react-native";
+import LoaderKit from "react-native-loader-kit";
+import { Track, useActiveTrack, useIsPlaying } from "react-native-track-player";
 import { TrackShortcutsMenu } from "./track-shortcut-menu";
+import { StopPropagation } from "./utils/stop-propagation";
 
 type PropsType = {
   item: {
@@ -25,6 +26,7 @@ export default function TrackListItem({
   onTrackSelected,
 }: PropsType) {
   const activeTrack = useActiveTrack();
+  const { playing } = useIsPlaying();
 
   const isActive = activeTrack?.title == item.title;
 
@@ -43,18 +45,33 @@ export default function TrackListItem({
           justifyContent: "flex-start",
         }}
       >
-        <Image
-          style={{
-            height: 50,
-            width: 50,
-            borderRadius: 8,
-            opacity: isActive ? 0.6 : 1,
-          }}
-          source={{
-            uri: item.artwork ?? unknownTrackImageUri,
-          }}
-        />
+        <View>
+          <Image
+            style={{
+              height: 50,
+              width: 50,
+              borderRadius: 8,
+              opacity: isActive ? 0.6 : 1,
+            }}
+            source={{
+              uri: item.artwork ?? unknownTrackImageUri,
+            }}
+          />
+          {isActive && playing && (
+            <LoaderKit
+              style={{
+                width: 30,
+                height: 30,
 
+                position: "absolute",
+                top: 10,
+                left: 10,
+              }}
+              name={"LineScaleParty"} // Optional: see list of animations below
+              color={"white"}
+            />
+          )}
+        </View>
         <View
           style={{
             flexDirection: "row",
